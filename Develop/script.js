24456653dd3129122fbf0c28c6f8930d57a36fee
Av720 <120581801+Av720@users.noreply.github.com>
2023-02-11 12:03:34 -0700

    added and refactored if and else statements for the prompt start

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,13 @@
 //  VARIABLES
 
-var lowerCase = "abcdefghijklmnopqrstuvwxyz" // lowerCase 
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // Uppercase 
-var numbers = "1234567890"; //Stricly numbers 
-var characters = "!@#$%^&*()_+'"; //All characters 
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"; // lowerCase
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // Uppercase
+var numbers = "1234567890"; //Stricly numbers
+var characters = "!@#$%^&*()_+'"; //All characters
 var randomSelection;
 var length;
 
-// target id (generate) element 
+// target id (generate) element
 var generateBtn = document.querySelector("#generate");
 
 function newPassword() {
@@ -15,109 +15,29 @@ function newPassword() {
   var passwordCharacters = document.querySelector("#password");
 }
 
-//Password parameters 
+//Password parameters
 
-function generateNewPassword();
-length = prompt("Enter the lenght of characters you would like for your new password *Number between 8 - 128*");
-if (!lengh) {
-  alert("Please try again! ")
+function generateNewPassword() {
+  // Prompt for the new password when clicked
+  length = prompt(
+    "Enter the lenght of characters you would like for your new password *Number between 8 - 128*"
+  );
+  // if there was a user error, "try again for a second time"
+  if (!length) {
+    alert("Please try again! ");
 
-} else if (lenght < 8 || lenght > 128) {
-  length = prompt("New password must contain a number from 8 - 128");
-
-} else {
-  numbers = confirm("Incude numbers in your password");
-  upperCase = confirm("Include the amount of upper case letters");
-  lowerCase = confirm("Iclude the amount of lower case letters");
-  characters = confirm("include the amount of characters in your password")
+    //setting the less and or more than for the charaters asked
+  } else if (length < 8 || length > 128) {
+    length = prompt("New password must contain a number from 8 - 128");
+  } else {
+    numbers = confirm("Incude numbers in your password");
+    upperCase = confirm("Include the amount of upper case letters");
+    lowerCase = confirm("Iclude the amount of lower case letters");
+    characters = confirm("include the amount of characters in your password");
+  }
 }
 
 
 
 
-
-
-
-
-//BUTTON PRESS TO GENERATE A PASSWORD 
-
-// function generatePassword()[
-
-//   length = prompt("Enter the number of characters you would like in your password *between 8 -28*");
-// if length 
-// ]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var click = document.getElementById("generate");
-// click.addEventListener("click", "generate");
-// functiondemo();
-
-// var generateBtn = document.getElementById("#generate")[0];
-// generate.addEventListener("click", function (event)) {
-//   alert("Choose a length of characters from 8 to 128");
-// }
-
-
-
-
-
-
-
-
-
-// Assignment Code
-
-
-
-// Declaring the variables
-
-
-// PROMPT for generating password 
-// var criteria = prompt("Please enter  ")
-
-
-
-
-
-// function generatePassword() {
-//   var criteria = 
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-// Defining the characters 
-
-
-
-
-
-
-// Write password to the #password input
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
+generateBtn.addEventListener("click", generateNewPassword);
