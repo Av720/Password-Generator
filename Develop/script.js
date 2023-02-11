047c2db4ecdb34cc019dbbb18602610010e28051
Av720 <120581801+Av720@users.noreply.github.com>
2023-02-11 11:36:27 -0700

    added if and else statments for the password parameters

--- Develop/script.js
+++ Develop/script.js
@@ -7,19 +7,30 @@ var characters = "!@#$%^&*()_+'"; //All characters
 var randomSelection;
 var length;
 
+// target id (generate) element 
+var generateBtn = document.querySelector("#generate");
 
+function newPassword() {
+  var password = generateNewPassword();
+  var passwordCharacters = document.querySelector("#password");
+}
 
+//Password parameters 
 
+function generateNewPassword();
+length = prompt("Enter the lenght of characters you would like for your new password *Number between 8 - 128*");
+if (!lengh) {
+  alert("Please try again! ")
 
+} else if (lenght < 8 || lenght > 128) {
+  length = prompt("New password must contain a number from 8 - 128");
 
-
-
-
-
-
-
-
-
+} else {
+  numbers = confirm("Incude numbers in your password");
+  upperCase = confirm("Include the amount of upper case letters");
+  lowerCase = confirm("Iclude the amount of lower case letters");
+  characters = confirm("include the amount of characters in your password")
+}
 
 
 
@@ -29,6 +40,29 @@ var length;
 
 
 //BUTTON PRESS TO GENERATE A PASSWORD 
+
+// function generatePassword()[
+
+//   length = prompt("Enter the number of characters you would like in your password *between 8 -28*");
+// if length 
+// ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // var click = document.getElementById("generate");
 // click.addEventListener("click", "generate");
 // functiondemo();
@@ -83,13 +117,6 @@ var length;
 
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
